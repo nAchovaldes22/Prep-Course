522b459eb1ffc59d2fe5e9f22ff9a03817c322ae
nAchovaldes22 <valdesjuanignacio@gmail.com>
2022-06-20 14:33:28 -0300

    segundo trabajo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -133,7 +133,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+} else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+} else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Numero 1 es mayor y positivo";
+} else if (num3 > num1 && num3 > num2) {
+    return num3 +1 ;
+}
+else {
+  return false;
+}  // tira errores en el test pero al probarlo en consola repl no me tira erroes. Hasta probando con los mismos numeros del test.
 }
 
 function esPrimo(numero) {
@@ -142,33 +153,59 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2) return false;
+  for (var i=2 ; i<numero; i++){
+    if(numero % i=== 0){
+      return false;
+    }
+  } return true; // tira errores en el test pero al probarlo en consola repl no me tira erroes. Hasta probando con los mismos numeros del test.
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor === true) {
+    return "Soy verdadero";
+  } else (valor === false) {
+    return "Soy falso";
+  }
+// tira errores en el test pero al probarlo en consola repl no me tira erroes. Hasta probando con los mismos numeros del test.
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
-}
+  var multiplosdelseis = []
+  for (let i = 0 ; i < 11 ; i++) {
+      multiplosdelseis.push(6*i)
+  }
+  return multiplosdelseis;// tira errores en el test pero al probarlo en consola repl no me tira erroes. Hasta probando con los mismos numeros del test.
+} 
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if(numero > 99 && numero < 1000){
+    return false
+  } else {
+    return true
+  }// tira errores en el test pero al probarlo en consola repl no me tira erroes. Hasta probando con los mismos numeros del test.
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-}
+  let veces=0;
+  let num=numero;
+  do{
+    num = num + 5;
+    veces = veces + 1;
+  } while (veces < 8);
+  return num;
+}// tira errores en el test pero al probarlo en consola repl no me tira erroes. Hasta probando con los mismos numeros del test.
 
 
 // No modificar nada debajo de esta línea
