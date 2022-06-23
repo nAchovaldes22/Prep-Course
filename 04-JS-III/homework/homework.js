9e601a8a0cadad1a4ff6ce080746774f3e95dccb
nAchovaldes22 <valdesjuanignacio@gmail.com>
2022-06-23 19:12:55 -0300

    array

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -89,10 +89,10 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  var suma=0;
+  var suma= 0;
   for (var i=0; i<resultadosTest.length; i++){
     suma = suma + resultadosTest[i];
-  } return suma/restultadosTest.length;
+  } return suma/ resultadosTest.length;
 }
 
 
@@ -102,7 +102,7 @@ function numeroMasGrande(numeros) {
   // Tu código:
   var masgrande = 0;
   for (var i=0; i<numeros.length; i++) {
-    if (masgrade< numeros[i]) {
+    if (masgrande< numeros[i]) {
       masgrande=numeros[i]
     } 
     } return masgrande;
@@ -124,7 +124,12 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  
+  var nuevoArray= 0;
+  for (var i=0; i< arreglo.length; i++) {
+    if(arreglo[i]>19) {
+      nuevoArray++
+    } 
+  } return nuevoArray;
 
 }
 
@@ -134,7 +139,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return "Es fin de semana";
+  } return "Es dia Laboral";
 } 
 
 
@@ -142,6 +149,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let empieza = n.toString();
+  if (empieza.charAt(0) !== "9") {
+    return false;
+  } return true; 
   
 }
 
@@ -150,6 +161,11 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var compara= arreglo[0];
+  for (let i=0; i< arreglo.length; i++) {
+    if (compara !== arreglo[i])
+    return false;
+  } return true;
   
 } 
 
@@ -159,6 +175,15 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let arraymeses = [];
+  for (let i = 0; i<array.length; i++) {
+    if ( array [i]=== "Enero" || array [i] === "Marzo" || array[i] === "Noviembre") {
+      arraymeses.push(array[i]);
+    } 
+  }
+  if(arraymeses.length< 3){
+    return "No se encontraron los meses pedidos";
+  } return arraymeses;
 }
 
 
@@ -166,6 +191,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var centenials= [];
+  for (var i=0; i< array.length; i++) {
+    if(array[i]>100) {
+      centenials.push(array[i])
+    } 
+  } return centenials;
 }
 
 
@@ -177,6 +208,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var arraysum = [];
+  var sum = numero
+  for (var i=0; i<10; i++) {
+    sum = sum + 2;
+    if (sum === i)break;
+    else {
+      arraysum.push(sum);
+    }
+  }  
+  if (i<10) {
+    return "Se interrumpió la ejecución";
+  }
+  return arraysum;
 }
 
 
@@ -187,6 +231,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var arraysum = [];
+  var sum = numero
+  for (var i=0; i<10; i++) {
+    if (i===5) continue;
+    else {
+      sum=sum+2;
+      arraysum.push(sum);
+    }
+  }
+  return arraysum;
 }
 
 
